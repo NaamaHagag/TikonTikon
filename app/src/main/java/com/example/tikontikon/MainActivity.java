93f7f44590510f5426c27a70ae6721c3a92f87c2
NaamaHagag <naamahagag29@gmail.com>
2024-01-27 22:01:12 +0200

    tikon

--- app/src/main/java/com/example/tikontikon/MainActivity.java
+++ app/src/main/java/com/example/tikontikon/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.tikontikon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=(Button) findViewById(R.id.button);
+        btn1.setText("Click me");
+        btn1.setTextSize(35);
+        btn1.setTextColor(Color.BLUE);
+        btn1.setBackgroundColor(Color.rgb(40,255,200));
     }
+
+    public void Boom(View view) {
+        count++;
+        String str= "This is a click number:" + count;
+        btn1.setText(str);
+        if (count%7==0){
+            btn1.setText("BOOM!");
+        }
+    }
+
+
+}
 }
